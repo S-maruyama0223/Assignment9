dfc38146b4b25f38c6bb44f0c9951df35f1de526
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2020-11-06 23:35:25 +0900

    initial commit

---
+++ Assignment9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment9
+//
+//  Created by Shotaro Maruyama on 2020/11/06.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
